bf7368350aff4e0885533c02c06937863bf4b2cd
Jessica D'Andrea <jessica.dandrea@pwc.com>
2017-07-12 21:07:53 -0500

    completed searchYouTube function, all tests passing

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,25 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var youTubeData = {
+    part: 'snippet',
+    q: options.query,
+    maxResults: options.max,
+    key: options.key,
+    type: 'video',
+    videoEmbeddable : true
+
+  }
+  $.ajax({
+  url: 'https://www.googleapis.com/youtube/v3/search',
+  type: 'GET',
+  data: youTubeData,
+  contentType: 'application/json',
+  success: function (data) {
+    callback(data.items);
+  },
+  error: function (data) {
+    console.error('error', data);
+  }
+});
 };
 
 window.searchYouTube = searchYouTube;
